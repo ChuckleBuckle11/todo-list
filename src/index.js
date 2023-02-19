ffca2b360de57559ac62dd697a1b19ac7138e31b
DLauWJ <102398736+ChuckleBuckle11@users.noreply.github.com>
2023-02-19 20:32:28 +0800

    Init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable indent */
+// To-do list app by Chucklebuckle
+
+// Brainstorm:
+// 1. to-do items should have:
+    // -title
+    // -description
+    // -dueDate
+    // -priority
+    // -notes
+    // -checklist
+// 2. Projects should be classes that to-dos get put into
+    // - users should be able to move to-dos between projects
+
+console.log('test');
