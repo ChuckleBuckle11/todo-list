a047cb8257197ed13180fc510d6a8855139fe14a
DLauWJ <102398736+ChuckleBuckle11@users.noreply.github.com>
2023-03-06 15:22:37 +0800

    Added ability to add tasks to main-content

--- src/modules/main-view.js
+++ src/modules/main-view.js
@@ -9,15 +9,17 @@ function selectProject(project){
     projectTitle.textContent = project.getTitle();
 
     const toDoContainer = document.createElement('div')
-    toDoContainer.textContent = "TODOS GO HERE"
-
+    toDoContainer.id = `to-do-container`;
+    toDoContainer.textContent = "TODOS GO HERE";
+    
     mainContent.appendChild(projectTitle);
     mainContent.appendChild(toDoContainer);
+
+    loadToDos(project);
     
     //flushes any active projects
     const projectList = document.querySelectorAll('.list-project');
     projectList.forEach((item) => {
-        console.log(item);
         item.classList.remove("active");
     })
     //Sets active class to current projects
@@ -25,5 +27,24 @@ function selectProject(project){
     projectinList.classList.add('active');
 }
 
+// Loads the to-dos in the main page
+function loadToDos(project){
+    console.log('test')
+    const toDoContainer = document.querySelector(`#to-do-container`);
+    let i = 0;
+    for (let task of project.getTasks()){
+        const toDo = document.createElement(`div`);
+        toDo.classList.add('to-do');
+        toDo.id = `to-do-${i};`
+        toDo.textContent = task.getTitle();
+
+
+        i++;
+
+        toDoContainer.appendChild(toDo);
+    }
+    
+
+}
 export {selectProject}
 
